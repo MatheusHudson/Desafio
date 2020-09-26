5f5b5a3f2c3cd93bbc87d04774b12ced2c872a1a
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2020-09-25 22:11:29 -0300

    ParteG
    Adicionando a classe DigitalHouseManager e seu uml.

---
+++ src/br/desafioCurso/com/DigitalHouseManger.kt
@@ -0,0 +1,8 @@
+package br.desafioCurso.com
+
+class DigitalHouseManger(
+        var listaAlunos: MutableList<Aluno>,
+        var listaProfessores: MutableList<Professor>,
+        var listaCursos: MutableList<Curso>,
+        var listaMatriculas: MutableList<Matricula>
+)
